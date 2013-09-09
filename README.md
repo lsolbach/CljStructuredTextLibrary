547d2255416c6afe7a542dddb5d041801c733b7b
Ludger Solbach <soulman@soulspace.org>
2013-09-09 09:06:42 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,16 @@
+CljStructuredTextLibrary
+========================
+The CljStructuredTextLibrary is a library for writing structured texts in clojure.
+
+Copyright
+---------
+© 2013 Ludger Solbach
+
+License
+-------
+[Eclipse Public License 1.0] (http://www.eclipse.org/legal/epl-v10.html "EPL 1.0")
+
+Code Repository
+---------------
+[https://github.com/lsolbach/CljStructuredTextLibrary] (https://github.com/lsolbach/CljStructuredTextLibrary)
+
---
+++ module.clj
@@ -0,0 +1,11 @@
+[
+ :module "CljStructuredTextLibrary"
+ :project "org.soulspace.clj"
+ :type :library
+ :version "0.1.0"
+ :description "The CljStructuredTextLibrary is a library for writing structured texts in clojure."
+ :provider "soulspace.org"
+ :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
+ :plugins ["global" "dependencies" "clojure" "clojuretest" "package" "release"]
+ :dependencies [["org.clojure/clojure, 1.5.1"]]
+ ]
